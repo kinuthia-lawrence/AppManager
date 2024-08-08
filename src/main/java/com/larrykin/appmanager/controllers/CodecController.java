67f6351650310c59352c9f11b06542114226ddfd
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-08-08 13:22:22 +0300

    modifed the ussd api ui

--- src/main/java/com/larrykin/appmanager/controllers/CodecController.java
+++ src/main/java/com/larrykin/appmanager/controllers/CodecController.java
@@ -1,4 +1,74 @@
 package com.larrykin.appmanager.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+
 public class CodecController {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Hyperlink deselectAllHyperlink;
+
+    @FXML
+    private CheckBox g722CheckBox;
+
+    @FXML
+    private CheckBox g723CheckBox;
+
+    @FXML
+    private CheckBox g728CheckBox;
+
+    @FXML
+    private CheckBox g729CheckBox;
+
+    @FXML
+    private CheckBox gsmCheckBox;
+
+    @FXML
+    private CheckBox h263CheckBox;
+
+    @FXML
+    private CheckBox h264CheckBox;
+
+    @FXML
+    private CheckBox ilbcCheckBox;
+
+    @FXML
+    private CheckBox l16_44_1CheckBox;
+
+    @FXML
+    private CheckBox l16_44_2CheckBox;
+
+    @FXML
+    private Button moveDownButton;
+
+    @FXML
+    private Button moveUpButton;
+
+    @FXML
+    private CheckBox mpeg4CheckBox;
+
+    @FXML
+    private CheckBox pcmaCheckBox;
+
+    @FXML
+    private CheckBox pcmuCheckBox;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Hyperlink selectAllHyperlink;
+
+    @FXML
+    private CheckBox speex_NarrowbandCheckBox;
+
+    @FXML
+    private CheckBox speex_widebandCheckBox;
+
+
 }
--- src/main/java/com/larrykin/appmanager/controllers/UssdApiController.java
+++ src/main/java/com/larrykin/appmanager/controllers/UssdApiController.java
@@ -1,4 +1,63 @@
 package com.larrykin.appmanager.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.util.Objects;
+
 public class UssdApiController {
+
+    @FXML
+    private Hyperlink DeselectAllHyperlink;
+
+    @FXML
+    private AnchorPane checkboxAnchorPane;
+
+    @FXML
+    private TableColumn<?, ?> dateGmtColumn;
+
+    @FXML
+    private ImageView iconrefresh;
+
+    @FXML
+    private TableColumn<?, ?> imeiColumn;
+
+    @FXML
+    private TableColumn<?, ?> lastResponseColumn;
+
+    @FXML
+    private Button refreshButton;
+
+    @FXML
+    private Hyperlink selectAllHyperlink;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private CheckBox sendToAllPhonesCheckBox;
+
+    @FXML
+    private TextField ussdCodeTextField;
+
+    @FXML
+    private Label warningLabel;
+
+    @FXML
+    public  void  initialize(){
+        loadReources();
+    }
+
+    private void loadReources() {
+        Image refreshImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/refresh.png")));
+        iconrefresh.setImage(refreshImage);
+    }
 }
