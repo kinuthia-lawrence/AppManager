efea4f484a3c038b68f43d71942faa5cc0ce18d3
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-08-08 13:57:39 +0300

    modified the whitelist and blackList ui

--- src/main/java/com/larrykin/appmanager/controllers/WhiteListAndBlacklistController.java
+++ src/main/java/com/larrykin/appmanager/controllers/WhiteListAndBlacklistController.java
@@ -1,4 +1,28 @@
 package com.larrykin.appmanager.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
 public class WhiteListAndBlacklistController {
+
+    @FXML
+    private TextField blacklistPathTextField;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private CheckBox enableBlackListRuleCheckBox;
+
+    @FXML
+    private CheckBox enableWhiteListRuleCheckBox;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField whitelistPathTextField;
+
 }
