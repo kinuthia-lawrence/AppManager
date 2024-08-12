f69a6aca4ec701eaf9172ca0b23a3ea5a7ec0245
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-08-12 11:08:21 +0300

    modified the register sip

--- src/main/java/com/larrykin/appmanager/controllers/registerSIPController.java
+++ src/main/java/com/larrykin/appmanager/controllers/registerSIPController.java
@@ -1,4 +1,43 @@
 package com.larrykin.appmanager.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class registerSIPController {
+
+    @FXML
+    private TextField authUsernameTextField;
+
+    @FXML
+    private TextField callerIDTextField;
+
+    @FXML
+    private TextField outBoundProxyTextField;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private TextField registrarServerTextfield;
+
+    @FXML
+    private TextField sipPasswordTextField;
+
+    @FXML
+    private TextField sipPortTextField;
+
+    @FXML
+    private TextField sipProxyTextField;
+
+    @FXML
+    private TextField sipServerTextField;
+
+    @FXML
+    private TextField sipUsernameTextField;
+
+    @FXML
+    private ComboBox<?> transportComboBox;
+
 }
